295b5987ebe30bd0162eb9ed664b34157efbf093
NexGenUA <mr.rinus@gmail.com>
2020-10-06 21:40:23 +0300

    feat: user endpoint implemented

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,38 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const User = require('./user.model');
+
+let users = [];
+
+const getAll = async () => users;
+
+const getUser = id => users.find(user => user.id === id);
+
+const createUser = data => {
+  const newUser = new User(data);
+  users.push(newUser);
+  return newUser;
 };
 
-module.exports = { getAll };
+const updateUser = (id, data) => {
+  const findUser = getUser(id);
+  let updatedUser = null;
+  if (findUser) {
+    users = users.map(user => {
+      if (user.id === id) {
+        updatedUser = { ...findUser, ...data };
+        return updatedUser;
+      }
+      return user;
+    });
+  }
+  return updatedUser;
+};
+
+const deleteUser = id => {
+  const deletedUser = getUser(id);
+  if (deletedUser) {
+    users = users.filter(user => user.id !== id);
+  }
+  return deleteUser;
+};
+
+module.exports = { getAll, getUser, createUser, updateUser, deleteUser };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,41 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
   res.json(users.map(User.toResponse));
 });
+
+router.route('/:id').get(async (req, res) => {
+  const user = await usersService.getUser(req.params.id);
+  if (user) {
+    return res.json(User.toResponse(user));
+  }
+  res.status(404).send('User not found');
+});
+
+router.route('/').post(async (req, res) => {
+  const user = await usersService.createUser(req.body);
+  res.json(User.toResponse(user));
+});
+
+router.route('/:id').put(async (req, res) => {
+  const user = await usersService.updateUser(req.params.id, req.body);
+  if (user) {
+    return res.json(User.toResponse(user));
+  }
+  res.status(404).send('Not Found');
+});
+
+router.route('/:id').delete(async (req, res) => {
+  const user = await usersService.deleteUser(req.id);
+  if (user) {
+    res.status(200).send('Succefull deleted');
+  }
+  res.status(404).send();
+});
+
+// * `GET /users/:id` - get the user by id (ex. “/users/123”) (remove password from response)
+// * `POST /users` - create user
+// * `PUT /users/:id` - update user
+// * `DELETE /users/:id` - delete user
 
 module.exports = router;
