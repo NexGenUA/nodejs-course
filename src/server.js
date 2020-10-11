0875dab9f1befb8b807be46e2e1f1ac2fe9b0aa8
NexGenUA <mr.rinus@gmail.com>
2020-10-11 20:16:10 +0300

    feat: add users mongoDB reppository

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
+const mongoConnect = require('./db/db-client');
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+mongoConnect()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`App is running on http://localhost:${PORT}`)
+    );
+  })
+  .catch(() => {
+    console.error('mongoDB connection error');
+  });
